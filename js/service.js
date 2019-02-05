5818afe41c039ae2ec548318de97b81692f049e4
Eitan Elnekave <eitan02581@gmail.com>
2019-02-05 19:36:48 +0200

    some

--- js/service.js
+++ js/service.js
@@ -6,7 +6,8 @@ function createProjs() {
         createProj('Mine-Sweeper', 'Mine Sweeper', 'Mine Sweeper', 'Nice game about discovering eliminating mines', 'projects/images/Mine-Sweeper.png', '30.01.19', ['matrix', 'game', 'events'], 'projects/Mine Sweeper/index.html'),
         createProj('Pacman', 'Pacman', 'Pacman', 'The old school game that everyone love!', 'projects/images/pacman.png', '27.01.19', ['matrix', 'game', 'events'], 'projects/pacman/index.html'),
         createProj('Chess', 'Chess', 'Chess', 'best thinking game - Chess!', 'projects/images/chess.png', '25.01.19', ['matrix', 'game', 'events'], 'projects/chess/index.html'),
-        createProj('Touch-nums', 'Touch-nums', 'Touch-nums', 'give a chance to your refleces', 'projects/images/touch-nums.png', '23.01.19', ['matrix', 'game', 'events'], 'projects/touch-nums/index.html')
+        createProj('Touch-nums', 'Touch-nums', 'Touch-nums', 'give a chance to your refleces', 'projects/images/touch-nums.png', '23.01.19', ['matrix', 'game', 'events'], 'projects/touch-nums/index.html'),
+        createProj('Book-shop', 'Book-shop', 'Book-shop', 'Take a look on the books', 'projects/images/book.png', '23.01.19', ['matrix', 'game', 'events'], 'projects/ex-book-shop/index.html')
     ]
 }
 
